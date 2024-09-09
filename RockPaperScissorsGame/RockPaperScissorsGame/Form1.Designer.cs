c82a91fb742637b5f6e3a27d1a5a5075d13c9994
Michael Joseph <mjwagn3@ilstu.edu>
2024-09-09 12:40:02 -0500

    first comment

---
+++ RockPaperScissorsGame/RockPaperScissorsGame/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace RockPaperScissorsGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scissorsPictureBox = new System.Windows.Forms.PictureBox();
+            this.paperPictureBox = new System.Windows.Forms.PictureBox();
+            this.rockPictureBox = new System.Windows.Forms.PictureBox();
+            this.selectPromptLabel = new System.Windows.Forms.Label();
+            this.rockPromptLabel = new System.Windows.Forms.Label();
+            this.paperPromptLabel = new System.Windows.Forms.Label();
+            this.scissorsPromptLabel = new System.Windows.Forms.Label();
+            this.shootButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.scissorsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paperPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rockPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scissorsPictureBox
+            // 
+            this.scissorsPictureBox.Image = global::RockPaperScissorsGame.Properties.Resources.scissors__1_;
+            this.scissorsPictureBox.Location = new System.Drawing.Point(342, 67);
+            this.scissorsPictureBox.Name = "scissorsPictureBox";
+            this.scissorsPictureBox.Size = new System.Drawing.Size(159, 200);
+            this.scissorsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.scissorsPictureBox.TabIndex = 2;
+            this.scissorsPictureBox.TabStop = false;
+            this.scissorsPictureBox.Click += new System.EventHandler(this.scissorsPictureBox_Click);
+            // 
+            // paperPictureBox
+            // 
+            this.paperPictureBox.Image = global::RockPaperScissorsGame.Properties.Resources.paper__1_;
+            this.paperPictureBox.Location = new System.Drawing.Point(177, 67);
+            this.paperPictureBox.Name = "paperPictureBox";
+            this.paperPictureBox.Size = new System.Drawing.Size(159, 200);
+            this.paperPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.paperPictureBox.TabIndex = 1;
+            this.paperPictureBox.TabStop = false;
+            this.paperPictureBox.Click += new System.EventHandler(this.paperPictureBox_Click);
+            // 
+            // rockPictureBox
+            // 
+            this.rockPictureBox.Image = global::RockPaperScissorsGame.Properties.Resources.theRock;
+            this.rockPictureBox.Location = new System.Drawing.Point(12, 67);
+            this.rockPictureBox.Name = "rockPictureBox";
+            this.rockPictureBox.Size = new System.Drawing.Size(159, 200);
+            this.rockPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rockPictureBox.TabIndex = 0;
+            this.rockPictureBox.TabStop = false;
+            this.rockPictureBox.Click += new System.EventHandler(this.rockPictureBox_Click);
+            // 
+            // selectPromptLabel
+            // 
+            this.selectPromptLabel.AutoSize = true;
+            this.selectPromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectPromptLabel.Location = new System.Drawing.Point(12, 9);
+            this.selectPromptLabel.Name = "selectPromptLabel";
+            this.selectPromptLabel.Size = new System.Drawing.Size(102, 20);
+            this.selectPromptLabel.TabIndex = 3;
+            this.selectPromptLabel.Text = "Click on One:";
+            // 
+            // rockPromptLabel
+            // 
+            this.rockPromptLabel.AutoSize = true;
+            this.rockPromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rockPromptLabel.Location = new System.Drawing.Point(8, 44);
+            this.rockPromptLabel.Name = "rockPromptLabel";
+            this.rockPromptLabel.Size = new System.Drawing.Size(50, 20);
+            this.rockPromptLabel.TabIndex = 4;
+            this.rockPromptLabel.Text = "Rock:";
+            // 
+            // paperPromptLabel
+            // 
+            this.paperPromptLabel.AutoSize = true;
+            this.paperPromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paperPromptLabel.Location = new System.Drawing.Point(173, 44);
+            this.paperPromptLabel.Name = "paperPromptLabel";
+            this.paperPromptLabel.Size = new System.Drawing.Size(55, 20);
+            this.paperPromptLabel.TabIndex = 5;
+            this.paperPromptLabel.Text = "Paper:";
+            // 
+            // scissorsPromptLabel
+            // 
+            this.scissorsPromptLabel.AutoSize = true;
+            this.scissorsPromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scissorsPromptLabel.Location = new System.Drawing.Point(338, 44);
+            this.scissorsPromptLabel.Name = "scissorsPromptLabel";
+            this.scissorsPromptLabel.Size = new System.Drawing.Size(73, 20);
+            this.scissorsPromptLabel.TabIndex = 6;
+            this.scissorsPromptLabel.Text = "Scissors:";
+            // 
+            // shootButton
+            // 
+            this.shootButton.Location = new System.Drawing.Point(531, 146);
+            this.shootButton.Name = "shootButton";
+            this.shootButton.Size = new System.Drawing.Size(75, 23);
+            this.shootButton.TabIndex = 7;
+            this.shootButton.Text = "Shoot!";
+            this.shootButton.UseVisualStyleBackColor = true;
+            this.shootButton.Click += new System.EventHandler(this.shootButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Programmed By Michael Wagner";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 315);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shootButton);
+            this.Controls.Add(this.scissorsPromptLabel);
+            this.Controls.Add(this.paperPromptLabel);
+            this.Controls.Add(this.rockPromptLabel);
+            this.Controls.Add(this.selectPromptLabel);
+            this.Controls.Add(this.scissorsPictureBox);
+            this.Controls.Add(this.paperPictureBox);
+            this.Controls.Add(this.rockPictureBox);
+            this.Name = "Form1";
+            this.Text = "Rock Paper Scissors Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.scissorsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paperPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rockPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox rockPictureBox;
+        private System.Windows.Forms.PictureBox paperPictureBox;
+        private System.Windows.Forms.PictureBox scissorsPictureBox;
+        private System.Windows.Forms.Label selectPromptLabel;
+        private System.Windows.Forms.Label rockPromptLabel;
+        private System.Windows.Forms.Label paperPromptLabel;
+        private System.Windows.Forms.Label scissorsPromptLabel;
+        private System.Windows.Forms.Button shootButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RockPaperScissorsGame/RockPaperScissorsGame/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Name: Michael Wagner
+ * Date: 4/17/2024
+ * ULID: MJWAGN3
+ * Assignment 6.9 Rock paper scissors game
+ * Description:
+ * This is a simply rock paper scissors game
+ */
+namespace RockPaperScissorsGame
+{
+    public partial class Form1 : Form
+    {
+        //varibles to check what choice the player has made and if they even made a choice
+        bool playerSelectedScissors = false, playerSelectedPaper = false, playerSelectedRock = false,playerHasSelected = false;
+        //will assign values to what each player has selected
+        int computerChoice = 0,playerChoice = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //when the program first loads the computer choice will be random generate between 1 to 3
+        //1 is paper, 2 is rock, 3 is scissors
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            computerChoice = ComputerChoice();
+        }
+        //a random number is generated here
+        private int ComputerChoice()
+        {
+            var rand = new Random();
+            return rand.Next(3) + 1;
+        }
+
+        //player clicks on the different picture boxs to determine choose, if any have be clicked before the game is determine and after the made their choise they cant undo it
+       //this is the rock choise
+        private void rockPictureBox_Click(object sender, EventArgs e)
+        {
+            if (!playerHasSelected)
+            {
+                playerHasSelected = true;
+                playerSelectedRock = true;
+                playerSelectedPaper = false;
+                playerSelectedScissors = false;
+                determinePlayerChoice();
+            }
+            else
+            {
+                MessageBox.Show("You have already made your choice and you will live with it!");
+            }
+        }
+        //this is the paper choice
+        private void paperPictureBox_Click(object sender, EventArgs e)
+        {
+
+            if (!playerHasSelected)
+            {
+                playerHasSelected = true;
+                playerSelectedRock = false;
+                playerSelectedPaper = true;
+                playerSelectedScissors = false;
+                determinePlayerChoice();
+
+            }
+            else
+            {
+                MessageBox.Show("You have already made your choice and you will live with it!");
+            }
+        }
+        //this is the scissors choice
+        private void scissorsPictureBox_Click(object sender, EventArgs e)
+        {
+
+            if (!playerHasSelected)
+            {
+                playerHasSelected = true;
+                playerSelectedRock = false;
+                playerSelectedPaper = false;
+                playerSelectedScissors = true;
+                determinePlayerChoice();
+
+            }
+            else
+            {
+                MessageBox.Show("You have already made your choice and you will live with it!");
+            }
+        }
+        //this is the button that will show outcome of game, if the player has not made a choice it will tell uuser to make choise else it will determine winner and make a new game
+        private void shootButton_Click(object sender, EventArgs e)
+        {
+            if (!playerHasSelected)
+            {
+                MessageBox.Show("You must choose. But choose wisely!");
+            }
+            else
+            {
+                MessageBox.Show(determineWinner());
+                newGame();
+
+                
+            }
+        }
+        //it will determine which choise the player has made and then comparing it to what the computer said it will then return a message indicating what was the outcome
+        private void determinePlayerChoice()
+        {
+            if (playerSelectedPaper)
+            {
+
+                playerChoice = 1;
+            }
+            else if (playerSelectedRock)
+            {
+                playerChoice = 2;
+            }else if (playerSelectedScissors)
+            {
+                playerChoice = 3;
+            }
+        }
+        private String determineWinner()
+        {
+            String message = "";
+            if(playerChoice == 1)
+            {
+                
+                switch (computerChoice)
+                {
+                    case 1:
+                        message = "You both have choosen paper and therefore tied!" + " The game must be played again to determine the winner!"; ;
+                        break;
+                    case 2:
+                        message = "You choosen paper and oppenent has choosen rock. Paper wraps rock, YOU WIN!";
+                        break;
+                    case 3:
+                        message = "You choosen paper and oppenent has choosen scissors. Scissors cuts paper, YOU LOSE!";
+                        break;
+
+                }
+            }
+            else if(playerChoice == 2)
+            {
+                switch (computerChoice)
+                {
+                    case 1: 
+                        message ="You have chosen rock and oppenent has choosen paper. Paper wraps rock, YOU LOSE!";
+                        break;
+                    case 2:
+                        message = "You both have choosen rock and therefore tied!"+" The game must be played again to determine the winner!";
+                        break;
+                    case 3:
+                        message = "You choosen rock and oppenent has choosen scissors. Rock smashes scissors, YOU WIN!";
+                        break;
+
+                }
+            }else if(playerChoice == 3)
+            {
+                switch (computerChoice)
+                {
+                    case 1:
+                        message = "You have choosen scissors and oppenent has choosen paper. Scissors cuts paper, YOU WIN!";
+                        break;
+                    case 2:
+                        message = "You have choose scissors and oppenent has choosen rock. Rock smashes scissors, YOU LOSE!";
+                        break;
+                    case 3:
+                        message = "You have both chosen scissors and therefore tied!" + " The game must be played again to determine the winner!"; ;
+                        break;
+                }
+
+            }
+            return message;
+        }
+        //this creates a resets all important values so the player can play again
+        public void newGame()
+        {
+            computerChoice = ComputerChoice();
+            playerHasSelected = false;
+            playerSelectedPaper = false;
+            playerSelectedRock = false;
+            playerSelectedScissors = false;
+            playerChoice = 0;
+        }
+    }
+}
